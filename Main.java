741613a04fa9d24dacebf0089f3f31dc3306ec00
chrispaul3000 <cristian.tolibas10@gmail.com>
2024-11-25 19:52:23 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,46 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pkg2narraytolibas;
-
-import java.util.Scanner;
+package string.concatenation;
 
 /**
  *
  * @author user
  */
-public class Main {
+public class StringConcatenation {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-
-      
-        System.out.print("Enter the number of rows: ");
-        int rows = scanner.nextInt();
-        System.out.print("Enter the number of columns: ");
-        int cols = scanner.nextInt();
-
-      
-        int[][] array = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                array[i][j] = i * j;
-            }
-        }
-        
-        System.out.println("Generated Multiplication Table:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(array[i][j] + "\t");
-            }
-            System.out.println();
-        }
-
-        scanner.close();
-    
+        String First = "Christian";
+        String Last = "Tolibas";
+        System.out.print("My name is" + First +" "+ Last);
     }
     
 }
